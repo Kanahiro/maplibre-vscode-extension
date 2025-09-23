d9e5019110a91e1296a3172ee8d3dce1e8d60617
Tom Konda <tom.konda.dev@gmail.com>
2025-09-23 13:10:10 +0900

    Upgrade ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import tsEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: [
+      'out',
+      'dist',
+      '**/*.d.ts',
+    ],
+  },
+  {
+    plugins: {
+      '@typescript-eslint': tsEslint,
+    },
+  },
+  {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+        '@typescript-eslint/naming-convention': [
+            'warn',
+            {
+                'selector': 'import',
+                'format': [ 'camelCase', 'PascalCase' ]
+            }
+        ],
+        'curly': 'warn',
+        'eqeqeq': 'warn',
+        'no-throw-literal': 'warn',
+        'semi': 'warn',
+    },
+  },
+];
